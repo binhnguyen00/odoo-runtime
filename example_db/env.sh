fbb3eed3fa8f844d02dbeb1c164ab2456a87f4bc
Binh Nguyen <jackjack2000.kahp@gmail.com>
2026-04-21 09:15:56 +0700

    update utils

--- example_db/env.sh
+++ example_db/env.sh
@@ -7,7 +7,7 @@ ADMIN_PASSWORD="admin"
 
 DB_NAME="dev"
 DB_ADMIN="dev"
-DB_PASSWORD="dev@123" 
+DB_PASSWORD="dev@123"
 DB_HOST="localhost"
 DB_PORT="5432"
 
@@ -20,6 +20,6 @@ DUMP_DIR="$WORKSPACE_DIR/runtime/$DB_NAME/db"
 DUMP_FILE="$DUMP_DIR/${DB_NAME}_${DATE}.tar"
 
 INIT_MODULES="accounting,hr"
-ODOO_VENV="$WORKSPACE_DIR/runtime/$DB_NAME/.venv"
+ODOO_VENV="$WORKSPACE_DIR/runtime/$DB_NAME/venv"
 
 DATA_DIR="$WORKSPACE_DIR/runtime/$DB_NAME/data"
