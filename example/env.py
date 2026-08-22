fe5099dab98ae6a3888bee938cb0d1fe4f4e9586
Binh Nguyen <jackjack2000.kahp@gmail.com>
2026-08-22 10:25:42 +0700

    fix bug

--- example/env.py
+++ example/env.py
@@ -8,9 +8,9 @@ Recommended Project Structure:
   └──runtime/
 """
 
-PROJECT_ROOT          = os.path.abspath(os.path.join(__file__, "../.."))  # path/to/your/project
-PROJECT_MODULES_DIR   = os.path.join(PROJECT_ROOT, "modules")             # your custom modules path
-RUNTIME_DIR           = os.path.join(PROJECT_ROOT, "runtime")             # runtime path
+PROJECT_ROOT          = "path/to/your/project"
+PROJECT_MODULES_DIR   = os.path.join(PROJECT_ROOT, "modules")  # your custom modules path
+RUNTIME_DIR           = os.path.join(PROJECT_ROOT, "runtime")  # runtime path
 
 ADMIN_DB              = "postgres"
 ADMIN_USER            = "postgres"
@@ -22,11 +22,11 @@ DB_PASSWORD           = "example@123"
 DB_HOST               = "localhost"
 DB_PORT               = "5432"
 
-DUMP_DIR              = "./db"
-DUMP_FILE             = f"{DUMP_DIR}/{DB_NAME}_{datetime.now().strftime('%Y%m%d%H%M%S')}.tar"
+DUMP_DIR              = os.path.join(RUNTIME_DIR, DB_NAME, "db")
+DUMP_FILE             = os.path.join(DUMP_DIR, f"{DB_NAME}_{datetime.now().strftime('%Y%m%d%H%M%S')}.tar")
 
-ODOO_DIR              = os.path.join(PROJECT_ROOT, "odoo")  # odoo source code path
-ODOO_CONFIG_FILE      = "./odoo.conf"                       # odoo config file path
-ODOO_INIT_MODULES     = "accounting,hr"                     # your modules name
-ODOO_VENV             = "./venv"                            # odoo venv path
-ODOO_DATA_DIR         = "./data"                            # odoo data path
+ODOO_DIR              = "/path/to/odoo/source/code"
+ODOO_INIT_MODULES     = "accounting,hr"  # modules to install/ update on server start
+ODOO_CONFIG_FILE      = os.path.join(RUNTIME_DIR, DB_NAME, "odoo.conf")
+ODOO_VENV             = os.path.join(RUNTIME_DIR, DB_NAME, "venv")
+ODOO_DATA_DIR         = os.path.join(RUNTIME_DIR, DB_NAME, "data")
